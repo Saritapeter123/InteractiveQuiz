674df25332cace9834fcf51e24b51fd2bce44d16
Sarita Peter <64759475+Saritapeter123@users.noreply.github.com>
2020-07-03 06:56:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Question array.
+var questions = [
+  { q: "DOM stands for: Document Object Model?", a: "t" },
+  { q: "Punctuation does not matter in Coding.", a: "f" },
+  { q: "HTML stands for: HyperText Markup Language?", a: "t" },
+  { q: "Do hummingbirds like to eat burgers?", a: "f" }
+];
+
+// Start the game with a score of 0.
+var score = 0;
+
+// Loop over every question. 
+for (var i = 0; i < questions.length; i++) {
+    
+}
