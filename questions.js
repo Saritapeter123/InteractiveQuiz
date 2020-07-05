b925a21e99e7a183499de724a212fe2321ab5288
Sarita Peter <64759475+Saritapeter123@users.noreply.github.com>
2020-07-05 14:15:03 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,40 @@
+  // Question array.
+  var questions = [
+    { q: "DOM stands for: Document Object Model?", a: "t" },
+    { q: "Punctuation does not matter in Coding.", a: "f" },
+    { q: "HTML stands for: HyperText Markup Language?", a: "t" },
+    { q: "Do hummingbirds like to eat burgers?", a: "f" }
+  ];
+  
+  // Start the game with a score of 0.
+  var score = 0;
+  
+  // Loop over every question. 
+  for (var i = 0; i < questions.length; i++) {
+    // Display question and ask for Ok/Cancel 
+    var answer = confirm(questions[i].q);
+  
+    // true or false
+    if ((answer === true && questions[i].a === "t") ||
+      (answer === false && questions[i].a === "f")) {
+      // increase score
+      score++;
+    }
+  }
+  // total at the end.
+  alert("You got " + score + "/" + questions.length);
+  
+  var questions = [
+    {
+      title: 'Commonly used data types DO NOT include:',
+      choices: ['strings', 'booleans', 'alerts', 'numbers'],
+      answer: 'alerts'
+    },
+    {
+      title: 'The condition in an if / else statement is enclosed within ____.',
+      choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+      answer: 'parentheses'
+    }
+    // etc.
+  ];
+  
--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-// Question array.
-var questions = [
-  { q: "DOM stands for: Document Object Model?", a: "t" },
-  { q: "Punctuation does not matter in Coding.", a: "f" },
-  { q: "HTML stands for: HyperText Markup Language?", a: "t" },
-  { q: "Do hummingbirds like to eat burgers?", a: "f" }
-];
+var time = document.querySelector("time");
 
-// Start the game with a score of 0.
-var score = 0;
+var secondsLeft = 75;
 
-// Loop over every question. 
-for (var i = 0; i < questions.length; i++) {
-  // Display question and ask for Ok/Cancel 
-  var answer = confirm(questions[i].q);
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    time.textContent = secondsLeft + " seconds left.";
 
-  // true or false
-  if ((answer === true && questions[i].a === "t") ||
-    (answer === false && questions[i].a === "f")) {
-    // increase score
-    score++;
-  }
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
 }
-// total at the end.
-alert("You got " + score + "/" + questions.length);
+
